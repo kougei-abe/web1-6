58eab6881f0027e022db8c52290d418cad154d81
kougei-abe <g1727005@st.t-kougei.ac.jp>
2018-06-04 18:15:54 +0900

    最初のコミット

---
+++ CatEscape/Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void LButtonDown()
+    {
+        transform.Translate(-3, 0, 0);
+    }
+    public void RButtonDown()
+    {
+        transform.Translate(3, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // 左矢印が押されたとき
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-3, 0, 0); // 左に｢3｣動かす
+        }
+
+        // 右矢印が押されたとき
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(3, 0, 0); // 右に｢3｣動かす
+        }
+	}
+}
